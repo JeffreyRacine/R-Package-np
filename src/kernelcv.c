ec9284064ab114b89c77be73e9e0824c5aa1985b
Jeffrey Racine <racinej@mcmaster.ca>
2011-07-27 14:55:40 -0400

    Reverting to previous code (unintended delete of two lines)

--- src/kernelcv.c
+++ src/kernelcv.c
@@ -214,6 +214,9 @@ double cv_func_regression_categorical_ls_nn(double *vector_scale_factor)
         return(DBL_MAX);
     }
 
+    py = &vector_Y_extern[0];
+    pm = &mean[0];
+
     for(i=0;i<num_obs_train_extern;i++)
     {
         cv += ipow((*py++ - *pm++),2);
