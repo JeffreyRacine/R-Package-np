ee59cba7bc1ac37bb79dc3795e5298d21eea4290
Jeffrey Racine <racinej@mcmaster.ca>
2012-07-16 16:05:22 -0400

    Changing interface to npcopula, two bandwidth objects, one for copula, one for copula density to avoid redundant computing

--- R/np.copula.R
+++ R/np.copula.R
@@ -5,37 +5,22 @@
 ## the copula `copula', the copula density `copula density' and u
 ## (columns for the grid constructed from the columns of the u matrix)
 
-npcopula <- function(bws.joint,data,bws.univariate=NULL,u=NULL) {
-
-  ## We make a copy of the joint bandwidth object then copy marginal
-  ## bandwidths into each position if supplied. All attributes of the
-  ## joint object are preserved. Note that for computational
-  ## efficiency we call npudist directly using tdat arguments so here
-  ## we must copy over all attributes.
-  
-  bws.marginal <- bws.joint  
+npcopula <- function(bws.copula,bws.copula.density,data,u=NULL) {
 
   ## Basic error checking
 
   if(missing(data)) stop("You must provide a data frame")
   if(!is.data.frame(data)) stop("Object `data' must be a data frame")
-  if(missing(bws.joint)) stop("You must provide a joint distribution bandwidth object")
+  if(missing(bws.copula)) stop("You must provide a joint distribution bandwidth object")
+  if(missing(bws.copula.density)) stop("You must provide a joint density bandwidth object")
   if(!is.null(u)) if(any(u>1) || any(u<0)) stop("u must lie in [0,1]")
-  num.var <- length(bws.joint$xnames)
+  if(length(bws.copula$xnames)!=length(bws.copula.density$xnames)) stop("bw.copula and bw.copula.density are not similar")
+  num.var <- length(bws.copula$xnames)
   
   ## Test for compatible quantile vector/matrix if provided
   if(!is.null(u)) {
     if(is.vector(u)) u <- matrix(u,1,length(u))
     if(ncol(u) != num.var) stop(paste("matrix u must have ", num.var," columns",sep=""))
-  }
-
-  ## If marginal bandwidth object list is provided copy each into
-  ## appropriate place in joint object
-
-  if(!is.null(bws.univariate)) {
-    if(!is.list(bws.univariate)) stop("bws.univariate must be a list containing bandwidth objects")
-    if(length(bws.univariate)!=num.var) stop(paste("bws.univariate must contain ", num.var, " bandwidth objects",sep=""))
-    for(j in 1:num.var) bws.marginal$bw[j] <- bws.univariate[[j]]$bw[1]
   }
 
   console <- newLineConsole()
@@ -45,25 +30,25 @@ npcopula <- function(bws.joint,data,bws.univariate=NULL,u=NULL) {
     ## realizations (joint CDF)
     console <- printPop(console)
     console <- printPush(msg = "Computing the copula at the sample realizations...", console)
-    copula <- fitted(npudist(bws=bws.joint,data=data))
+    copula <- fitted(npudist(bws=bws.copula,data=data))
     console <- printPop(console)
     console <- printPush(msg = "Computing the copula density at the sample realizations...", console)
-    copula.density <- fitted(npudens(bws=bws.joint,data=data))    
+    copula.density <- fitted(npudens(bws=bws.copula.density,data=data))    
     ## Compute the marginal quantiles from the marginal CDFs (u_i=\hat
     ## F(x_i)) and divide copula density by its marginals
-    u <- matrix(NA,bws.joint$nobs,num.var)
+    u <- matrix(NA,bws.copula$nobs,num.var)
     for(j in 1:num.var) {
       console <- printPop(console)
-      console <- printPush(msg = paste("Computing the marginal of ",bws.joint$xnames[j]," at the sample realizations...",sep=""), console)
-      bws.F <- npudensbw(formula(paste("~",bws.joint$xnames[j])),
-                         bws=bws.marginal$bw[j],
+      console <- printPush(msg = paste("Computing the marginal of ",bws.copula.density$xnames[j]," at the sample realizations...",sep=""), console)
+      bws.F <- npudensbw(formula(paste("~",bws.copula.density$xnames[j])),
+                         bws=bws.copula.density$bw[j],
                          bandwidth.compute=FALSE,
-                         bwmethod=bws.marginal$method,
-                         bwtype=bws.marginal$type,
-                         ckerorder=bws.marginal$ckerorder,
-                         ckertype=bws.marginal$ckertype,
-                         okertype=bws.marginal$okertype,
-                         ukertype=bws.marginal$ukertype,
+                         bwmethod=bws.copula.density$method,
+                         bwtype=bws.copula.density$type,
+                         ckerorder=bws.copula.density$ckerorder,
+                         ckertype=bws.copula.density$ckertype,
+                         okertype=bws.copula.density$okertype,
+                         ukertype=bws.copula.density$ukertype,
                          data=data)
 
       u[,j] <- fitted(npudist(bws=bws.F,data=data))
@@ -71,15 +56,15 @@ npcopula <- function(bws.joint,data,bws.univariate=NULL,u=NULL) {
       ## to have the same bws in numerator and denominator, so use
       ## those from the joint (mirror regression, conditional density
       ## estimation etc.)
-      bws.f <- npudensbw(formula(paste("~",bws.joint$xnames[j])),
-                         bws=bws.joint$bw[j],
+      bws.f <- npudensbw(formula(paste("~",bws.copula.density$xnames[j])),
+                         bws=bws.copula.density$bw[j],
                          bandwidth.compute=FALSE,
-                         bwmethod=bws.marginal$method,
-                         bwtype=bws.marginal$type,
-                         ckerorder=bws.marginal$ckerorder,
-                         ckertype=bws.marginal$ckertype,
-                         okertype=bws.marginal$okertype,
-                         ukertype=bws.marginal$ukertype,
+                         bwmethod=bws.copula.density$method,
+                         bwtype=bws.copula.density$type,
+                         ckerorder=bws.copula.density$ckerorder,
+                         ckertype=bws.copula.density$ckertype,
+                         okertype=bws.copula.density$okertype,
+                         ukertype=bws.copula.density$ukertype,
                          data=data)
       copula.density <- copula.density/NZD(fitted(npudens(bws=bws.f,data=data)))
     }
@@ -91,7 +76,7 @@ npcopula <- function(bws.joint,data,bws.univariate=NULL,u=NULL) {
     x.u <- matrix(NA,n.u,num.var)
     for(j in 1:num.var) {
       console <- printPop(console)
-      console <- printPush(msg = paste("Computing the pseudo-inverse for the marginal of ",bws.joint$xnames[j],"...",sep=""), console)
+      console <- printPush(msg = paste("Computing the pseudo-inverse for the marginal of ",bws.copula$xnames[j],"...",sep=""), console)
       ## Compute the quasi inverse (Definition 2.3.6, Nelson
       ## (2006)).  Here we take pains to span a sufficiently rich
       ## set of evaluation points to cover a range of
@@ -105,20 +90,20 @@ npcopula <- function(bws.joint,data,bws.univariate=NULL,u=NULL) {
       ## data again to provide a sufficiently fine grid.  We then
       ## concatenate and sort the equally space extended grid and
       ## the equi-quantile grid.
-      x.marginal <- eval(parse(text=paste("data$",bws.joint$xnames[j],sep="")))
+      x.marginal <- eval(parse(text=paste("data$",bws.copula$xnames[j],sep="")))
       x.er <- extendrange(x.marginal,f=1)
       x.q <- quantile(x.marginal,seq(0,1,length=500))
       x.eval <- sort(c(seq(x.er[1],x.er[2],length=500),x.q))
       ## Compute the CDF at this set of evaluation points
       F <- fitted(npudist(tdat=x.marginal,
                           edat=x.eval,
-                          bws=bws.marginal$bw[j],
-                          bwmethod=bws.marginal$method,
-                          bwtype=bws.marginal$type,
-                          ckerorder=bws.marginal$ckerorder,
-                          ckertype=bws.marginal$ckertype,
-                          okertype=bws.marginal$okertype,
-                          ukertype=bws.marginal$ukertype,data=data))
+                          bws=bws.copula$bw[j],
+                          bwmethod=bws.copula$method,
+                          bwtype=bws.copula$type,
+                          ckerorder=bws.copula$ckerorder,
+                          ckertype=bws.copula$ckertype,
+                          okertype=bws.copula$okertype,
+                          ukertype=bws.copula$ukertype,data=data))
       ## Now compute the psuedo inverse from the estimated F for the
       ## evaluation points
       for(i in 1:n.u) {
@@ -132,32 +117,32 @@ npcopula <- function(bws.joint,data,bws.univariate=NULL,u=NULL) {
     console <- printPop(console)
     console <- printPush(msg = "Expanding the u matrix and computing the copula and copula density...", console)
     x.u <- expand.grid(data.frame(x.u))
-    names(x.u) <- bws.joint$xnames
+    names(x.u) <- bws.copula$xnames
     console <- printPop(console)
     console <- printPush(msg = "Computing the copula at the expanded grid...", console)
-    copula <- predict(npudist(bws=bws.joint),data=data,newdata=x.u)
+    copula <- predict(npudist(bws=bws.copula),data=data,newdata=x.u)
     console <- printPop(console)
     console <- printPush(msg = "Computing the copula density at the expanded grid...", console)
-    copula.density <- predict(npudens(bws=bws.joint),data=data,newdata=x.u)
+    copula.density <- predict(npudens(bws=bws.copula.density),data=data,newdata=x.u)
     ## For the copula density require marginal densities. Desirable to
     ## have the same bws in numerator and denominator, so use those
     ## from the joint (mirror regression, conditional density
     ## estimation etc.)
     for(j in 1:num.var) {
       console <- printPop(console)
-      console <- printPush(msg = paste("Computing the marginal of ",bws.joint$xnames[j]," at the expanded grid...",sep=""), console)
-      bws.f <- npudensbw(formula(paste("~",bws.joint$xnames[j])),
-                         bws=bws.joint$bw[j],
+      console <- printPush(msg = paste("Computing the marginal of ",bws.copula.density$xnames[j]," at the expanded grid...",sep=""), console)
+      bws.f <- npudensbw(formula(paste("~",bws.copula.density$xnames[j])),
+                         bws=bws.copula.density$bw[j],
                          bandwidth.compute=FALSE,
-                         bwmethod=bws.marginal$method,
-                         bwtype=bws.marginal$type,
-                         ckerorder=bws.marginal$ckerorder,
-                         ckertype=bws.marginal$ckertype,
-                         okertype=bws.marginal$okertype,
-                         ukertype=bws.marginal$ukertype,
+                         bwmethod=bws.copula.density$method,
+                         bwtype=bws.copula.density$type,
+                         ckerorder=bws.copula.density$ckerorder,
+                         ckertype=bws.copula.density$ckertype,
+                         okertype=bws.copula.density$okertype,
+                         ukertype=bws.copula.density$ukertype,
                          data=data)
       xeval <- data.frame(x.u[,j])
-      names(xeval) <- bws.joint$xnames[j]
+      names(xeval) <- bws.copula.density$xnames[j]
       ## Divide copula density by its marginals
       copula.density <- copula.density/NZD(predict(npudens(bws=bws.f,data=data),newdata=xeval))
     }
