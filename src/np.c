b59e605642bf08e2f5a7c69efc4400f203d7e4b8
Tristen Hayfield <tristen.hayfield@gmail.com>
2013-11-05 17:23:54 +0100

    fixed a regression regression

--- src/np.c
+++ src/np.c
@@ -3851,7 +3851,7 @@ void np_regression(double * tuno, double * tord, double * tcon, double * ty,
         matrix_X_continuous_eval_extern[j][i]=econ[j*num_obs_eval_extern+i];
   }
 
-  if (vector_Y_eval_extern != NULL)
+  if (!ey_is_ty)
     for(i=0;i<num_obs_eval_extern;i++)
       vector_Y_eval_extern[i] = ey[i];
 
@@ -3933,7 +3933,7 @@ void np_regression(double * tuno, double * tord, double * tcon, double * ty,
         for( i=0;i<num_obs_eval_extern;i++ )
           matrix_X_continuous_eval_extern[j][i]=econ[j*num_obs_eval_extern+ipe[i]];
 
-      if(vector_Y_eval_extern != NULL)
+      if(!ey_is_ty)
         for(i=0;i<num_obs_eval_extern;i++)
           vector_Y_eval_extern[i] = ey[ipe[i]];
 
