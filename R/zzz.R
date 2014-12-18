1ee19a03b3d792f6ca08a8667a876938f81a04fb
Jeffrey Racine <racinej@mcmaster.ca>
2014-12-18 11:52:02 -0500

    Use of .onLoad suggested (by Bernd Bischl, pertaining to crs but also valid for np)

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,14 @@
 .onAttach <- function (lib, pkg) {
 	packageStartupMessage("Nonparametric Kernel Methods for Mixed Datatypes (version 0.60-2)\n[vignette(\"np_faq\",package=\"np\") provides answers to frequently asked questions]\n[vignette(\"np\",package=\"np\") an overview]\n[vignette(\"entropy_np\",package=\"np\") an overview of entropy-based methods]", domain = NULL,  appendLF = TRUE)
+}
 
+.onLoad <- function (lib, pkg) {
   if(is.null(options('np.messages')$np.messages))
     options(np.messages = TRUE)
-
   if(is.null(options('np.tree')$np.tree))
     options(np.tree = FALSE)
-
 }
+
 .onUnload <- function (lpath){
   library.dynam.unload("np", libpath=lpath) 
 }
