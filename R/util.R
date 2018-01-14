6a66a5ddf4a66db292f86fde3aa4e6d0eb3009fc
Jeffrey Racine <racinej@mcmaster.ca>
2018-01-14 10:26:06 -0500

    checking for finite correction factor in integrate.trapezoidal

--- R/util.R
+++ R/util.R
@@ -9,13 +9,16 @@ integrate.trapezoidal <- function(x,y) {
   y <- y[order.x]
   x <- x[order.x]
   int.vec <- numeric(length(x))
-  ## Use a correction term at the boundary: -cx^2/12*(f'(b)-f'(a))
+  ## Use a correction term at the boundary: -cx^2/12*(f'(b)-f'(a)),
+  ## check for NaN case
   cx  <- x[2]-x[1]
   ca <- (y[2]-y[1])/cx
   cb <- (y[n]-y[n-1])/cx
+  cf <- cx^2/12*(cb-ca)
+  if(!is.finite(cf)) cf <- 0
   int.vec[1] <- 0
   int.vec[2:n] <- cumsum((x[2:n]-x[2:n-1])*(y[2:n]+y[2:n-1])/2)
-  return(int.vec[rank.x]-cx^2/12*(cb-ca))
+  return(int.vec[rank.x]-cf)
 }
 
 ## No Zero Denominator, used in C code for kernel estimation...
