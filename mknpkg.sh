4d90fbcb7b5d507c573d08692c2c2aa49e4a1bd6
Jeffrey Racine <racinej@mcmaster.ca>
2026-02-07 20:49:03 -0500

    Fix(shellcheck): Address shellcheck warnings in cleanup, configure, mknpkg.sh, and inst scripts

--- mknpkg.sh
+++ mknpkg.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-maver=`cat npver`
+maver=$(cat npver)
 miver=${maver##*-}
 maver=${maver%-*}
 
-if [ $1 ] 
-    then 
-    let 'miver+=1' 
-    echo $maver-$miver > npver
+if [ -n "$1" ]; then 
+    miver=$((miver + 1)) 
+    echo "$maver-$miver" > npver
 fi
 
 echo updating DESCRIPTION
-sed -i.bak -e 's/Version[:].*/'"Version: $maver-$miver/" -e 's/Date[:].*/'"Date: `date +%Y-%m-%d`/" DESCRIPTION
+sed -i.bak -e "s/Version[:].*/Version: $maver-$miver/" -e "s/Date[:].*/Date: $(date +%Y-%m-%d)/" DESCRIPTION
 
 rm DESCRIPTION.bak
 
-cd R
+cd R || exit
 
 echo updating zzz.R
-sed -i.bak -e 's/version [0-9.][0-9.]*-[0-9][0-9]*/'"version $maver-$miver/" zzz.R 
+sed -i.bak -e "s/version [0-9.][0-9.]*-[0-9][0-9]*/version $maver-$miver/" zzz.R 
 
 rm zzz.R.bak
 
-cd ..
+cd .. || exit
