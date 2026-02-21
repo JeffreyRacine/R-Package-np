39ae4188f2625bba6b3d7f0f21298e9235664a51
Jeffrey Racine <racinej@mcmaster.ca>
2026-02-21 15:53:45 -0500

    plot: remove npplot S3 dispatch registrations
    - replace npplot() UseMethod path with explicit compatibility dispatcher\n- drop S3method(npplot, ...) registrations from NAMESPACE\n- retain direct npplot() compatibility behavior with advisory

--- R/np.plot.npplot.generic.R
+++ R/np.plot.npplot.generic.R
@@ -18,7 +18,6 @@ npplot <- function(bws = stop("'bws' has not been set"), ..., random.seed = 42,
   if (!isTRUE(.npplot.internal))
     .np_warn_npplot_deprecated_once()
 
-  seed.state <- .np_seed_enter(random.seed)
-  on.exit(.np_seed_exit(seed.state), add = TRUE)
-  UseMethod("npplot", bws)
+  .np_plot_call_method(.np_plot_compat_dispatch, bws = bws, ...,
+                       random.seed = random.seed)
 }
