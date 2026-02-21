e64ecfafbcc9031c2b064665c9832c8aea7442b7
Jeffrey Racine <racinej@mcmaster.ca>
2026-02-21 10:25:11 -0500

    refactor(core): replace tbw eval(parse) dispatch with do.call across np* core estimators

--- R/np.density.R
+++ R/np.density.R
@@ -256,8 +256,13 @@ npudens.default <- function(bws, tdat, ...){
   tbw <- eval.parent(sc.bw)
 
   ## convention: first argument is always dropped, second, if present, propagated
-  eval(parse(text=paste("npudens(bws = tbw",
-               ifelse(no.tdat, "",
-                      ifelse(tdat.named, ",tdat = tdat",",tdat")),
-               ",...)")))
+  call.args <- list(bws = tbw)
+  if (!no.tdat) {
+    if (tdat.named) {
+      call.args$tdat <- tdat
+    } else {
+      call.args <- c(call.args, list(tdat))
+    }
+  }
+  do.call(npudens, c(call.args, list(...)))
 }
