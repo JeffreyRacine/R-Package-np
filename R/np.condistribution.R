97a49fad63e4dd8c908bcc352e2328fc8ed2bba0
Jeffrey Racine <racinej@mcmaster.ca>
2026-02-22 17:49:26 -0500

    modernize core formula model.frame dispatch
    Replace eval(tmf, ...) with do.call('model.frame', mf.args, ...) in core formula methods (density/condensity/distribution/condistribution/qreg/conmode).
    
    Validation: testthat::test_local(filter='formula|density|distribution|qreg|conmode'); serial smoke script /tmp/repro_formula_core_np_master_20260222.R (ok=TRUE).

--- R/np.condistribution.R
+++ R/np.condistribution.R
@@ -28,7 +28,8 @@ npcdist.formula <-
     tmf <- bws$call[c(1,m)]
     tmf[[1]] <- as.name("model.frame")
     tmf[["formula"]] <- tt
-    umf <- tmf <- eval(tmf, envir = environment(tt))
+    mf.args <- as.list(tmf)[-1L]
+    umf <- tmf <- do.call("model.frame", mf.args, envir = environment(tt))
 
     tydat <- tmf[, bws$variableNames[["response"]], drop = FALSE]
     txdat <- tmf[, bws$variableNames[["terms"]], drop = FALSE]
