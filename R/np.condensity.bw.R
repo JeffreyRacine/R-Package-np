d046574980a6088d33f4d106c7c71a24985ba425
Jeffrey Racine <racinej@mcmaster.ca>
2026-02-22 13:18:59 -0500

    Modernize npcdensbw.formula model.frame eval path

--- R/np.condensity.bw.R
+++ R/np.condensity.bw.R
@@ -79,7 +79,19 @@ npcdensbw.formula <-
       attr(mf[["formula"]], "predvars") <- bquote(.(as.call(c(quote(cbind),as.call(c(quote(as.data.frame),as.call(c(quote(ts.intersect), arguments.timeseries)))),arguments.normal,check.rows = TRUE)))[,.(ix)])
     }
 
-    mf <- eval(mf, parent.frame())
+    mf.args <- as.list(mf[-1L])
+    has.data <- !is.null(mf.args$data)
+    mf.args$formula <- eval(mf.args$formula, envir = parent.frame())
+    if (has.data)
+      mf.args$data <- eval(mf.args$data, envir = parent.frame())
+    if (!is.null(mf.args$na.action))
+      mf.args$na.action <- eval(mf.args$na.action, envir = parent.frame())
+    if (!is.null(mf.args$subset))
+      mf.args$subset <- if (has.data)
+        eval(mf.args$subset, envir = mf.args$data, enclos = parent.frame())
+      else
+        eval(mf.args$subset, envir = parent.frame())
+    mf <- do.call("model.frame", mf.args, envir = parent.frame())
 
     ydat <- mf[, variableNames[[1]], drop = FALSE]
     xdat <- mf[, variableNames[[2]], drop = FALSE]
--- tests/testthat/test-formula-bw-contract.R
+++ tests/testthat/test-formula-bw-contract.R
@@ -142,3 +142,31 @@ test_that("formula npcdistbw gdata path matches default interface", {
 
   expect_equal(as.numeric(bw_formula$bw), as.numeric(bw_default$bw))
 })
+
+test_that("formula npcdensbw matches default interface with subset/na.action", {
+  set.seed(20260222)
+  dat <- data.frame(
+    y = rnorm(36),
+    x = runif(36)
+  )
+  dat$y[c(5, 19)] <- NA_real_
+
+  bw_formula <- np::npcdensbw(
+    y ~ x,
+    data = dat,
+    subset = x > 0.25,
+    na.action = na.omit,
+    bws = c(0.35, 0.45),
+    bandwidth.compute = FALSE
+  )
+
+  mf <- model.frame(y ~ x, data = dat, subset = x > 0.25, na.action = na.omit)
+  bw_default <- np::npcdensbw(
+    xdat = mf[, "x", drop = FALSE],
+    ydat = mf[, "y", drop = FALSE],
+    bws = c(0.35, 0.45),
+    bandwidth.compute = FALSE
+  )
+
+  expect_equal(as.numeric(bw_formula$bw), as.numeric(bw_default$bw))
+})
