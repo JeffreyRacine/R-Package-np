7768a214df2d04a27fe44b37357a7fc0f5b682ed
Jeffrey Racine <racinej@mcmaster.ca>
2026-02-22 19:10:54 -0500

    fix(spawn): qualify slave daemon MPI calls in namespace

--- inst/slavedaemon.R
+++ inst/slavedaemon.R
@@ -5,31 +5,31 @@ if (!library(npRmpi,logical.return = TRUE)){
 options(error=quote(assign(".mpi.err", TRUE, envir = .GlobalEnv)))
 .comm <- 1
 .intercomm <- 2
-invisible(mpi.comm.get.parent(.intercomm))
-invisible(mpi.intercomm.merge(.intercomm,1,.comm))
-invisible(mpi.comm.set.errhandler(.comm))
-mpi.hostinfo(.comm)
-invisible(mpi.comm.disconnect(.intercomm))
-.nonblock <- as.logical(mpi.bcast(integer(1),type=1,rank=0,comm=.comm))
-.sleep <- mpi.bcast(double(1),type=2,rank=0,comm=.comm)
+invisible(npRmpi:::mpi.comm.get.parent(.intercomm))
+invisible(npRmpi:::mpi.intercomm.merge(.intercomm,1,.comm))
+invisible(npRmpi:::mpi.comm.set.errhandler(.comm))
+npRmpi:::mpi.hostinfo(.comm)
+invisible(npRmpi:::mpi.comm.disconnect(.intercomm))
+.nonblock <- as.logical(npRmpi:::mpi.bcast(integer(1),type=1,rank=0,comm=.comm))
+.sleep <- npRmpi:::mpi.bcast(double(1),type=2,rank=0,comm=.comm)
 repeat {
-	tmp.message=mpi.bcast.cmd(rank=0,comm=.comm, nonblock=.nonblock, sleep=.sleep)
+	tmp.message=npRmpi:::mpi.bcast.cmd(rank=0,comm=.comm, nonblock=.nonblock, sleep=.sleep)
 	if (is.character(tmp.message) && tmp.message =="kaerb")
 		break
     try(eval(tmp.message,envir=.GlobalEnv),TRUE)
 }
 print("Done")
-if (mpi.comm.size(.comm) > 0) {
+if (npRmpi:::mpi.comm.size(.comm) > 0) {
     # `.comm` is an intracommunicator (created via `MPI_Intercomm_merge()`).
     # Prefer `MPI_Comm_free()`; `MPI_Comm_disconnect()` can destabilize
     # subsequent spawn/merge cycles with MPICH.
     if (is.loaded("mpi_comm_free")) {
-        invisible(mpi.comm.free(.comm))
+        invisible(npRmpi:::mpi.comm.free(.comm))
     } else if (is.loaded("mpi_comm_disconnect")) {
-        invisible(mpi.comm.disconnect(.comm))
+        invisible(npRmpi:::mpi.comm.disconnect(.comm))
     } else {
-        invisible(mpi.comm.free(.comm))
+        invisible(npRmpi:::mpi.comm.free(.comm))
     }
 }
-invisible(mpi.comm.set.errhandler(0))
-mpi.quit()
+invisible(npRmpi:::mpi.comm.set.errhandler(0))
+npRmpi:::mpi.quit()
