9d2d2b8e618be3aae3fa7b5e19a966a5b4cf3dd1
Jeffrey Racine <racinej@mcmaster.ca>
2026-02-26 07:14:24 -0500

    plot defaults: wild for regression-class, inid for unsupervised

--- tests/testthat/test-plot-autodispatch.R
+++ tests/testthat/test-plot-autodispatch.R
@@ -86,3 +86,30 @@ test_that("autodispatch default condensity plot options stay scalar-safe", {
   on.exit(dev.off(), add = TRUE)
   expect_error(plot(fit, perspective = FALSE), NA)
 })
+
+test_that("plot engine defaults: regression-class uses wild, unsupervised uses inid", {
+  reg.engines <- c(
+    ".np_plot_rbandwidth_engine",
+    ".np_plot_scbandwidth_engine",
+    ".np_plot_plbandwidth_engine",
+    ".np_plot_sibandwidth_engine"
+  )
+  unsup.engines <- c(
+    ".np_plot_bandwidth_engine",
+    ".np_plot_dbandwidth_engine",
+    ".np_plot_conbandwidth_engine",
+    ".np_plot_condbandwidth_engine"
+  )
+
+  for (nm in reg.engines) {
+    fn <- getFromNamespace(nm, "npRmpi")
+    defaults <- eval(formals(fn)$plot.errors.boot.method)
+    expect_identical(defaults[1L], "wild")
+  }
+
+  for (nm in unsup.engines) {
+    fn <- getFromNamespace(nm, "npRmpi")
+    defaults <- eval(formals(fn)$plot.errors.boot.method)
+    expect_identical(defaults[1L], "inid")
+  }
+})
