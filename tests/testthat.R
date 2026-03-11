1397895ba66f8af53acab4c71053e8d5a363b8ce
Jeffrey Racine <racinej@mcmaster.ca>
2026-03-10 21:33:42 -0400

    test: unload namespace after check suite

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,6 +5,12 @@ local({
   on.exit({
     try(npRmpi.quit(force = TRUE), silent = TRUE)
     try(mpi.finalize(), silent = TRUE)
+    if ("package:npRmpi" %in% search()) {
+      try(detach("package:npRmpi", unload = TRUE, character.only = TRUE), silent = TRUE)
+    }
+    if ("npRmpi" %in% loadedNamespaces()) {
+      try(unloadNamespace("npRmpi"), silent = TRUE)
+    }
   }, add = TRUE)
   test_check("npRmpi")
 })
