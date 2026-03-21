54ac7c308063e2c353cede4d66e6bcc3353b767c
Jeffrey Racine <racinej@mcmaster.ca>
2026-03-21 13:50:00 -0400

    Stop stamping startup version in release script

--- mknpkg.sh
+++ mknpkg.sh
@@ -14,12 +14,3 @@ echo updating DESCRIPTION
 sed -i.bak -e 's/Version[:].*/'"Version: $maver-$miver/" -e 's/Date[:].*/'"Date: $(date +%Y-%m-%d)/" DESCRIPTION
 
 rm DESCRIPTION.bak
-
-(cd R || exit 1
-
-echo updating zzz.R
-sed -i.bak -e 's/version [0-9.][0-9.]*-[0-9][0-9]*/'"version $maver-$miver/" zzz.R 
-
-rm zzz.R.bak
-
-) # end subshell
