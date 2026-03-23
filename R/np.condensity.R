7f1f91cc3871aa85d7c946ffe264612dc890e425
Jeffrey Racine <racinej@mcmaster.ca>
2026-03-22 22:25:58 -0400

    Add conditional nomad convenience shortcuts

--- R/np.condensity.R
+++ R/np.condensity.R
@@ -394,9 +394,10 @@ npcdens.conbandwidth <- function(bws,
 }
 
 
-npcdens.default <- function(bws, txdat, tydat, ...){
+npcdens.default <- function(bws, txdat, tydat, nomad = FALSE, ...){
   sc <- sys.call()
   sc.names <- names(sc)
+  nomad <- npValidateScalarLogical(nomad, "nomad")
 
   ## here we check to see if the function was called with tdat =
   ## if it was, we need to catch that and map it to dat =
---
+++ tests/testthat/test-npcd-nomad-shortcut-contract.R
@@ -0,0 +1,143 @@
+test_that("npcdens nomad shortcut matches the explicit density preset", {
+  skip_if_not_installed("crs")
+
+  old_opts <- options(np.messages = FALSE, np.tree = FALSE)
+  on.exit(options(old_opts), add = TRUE)
+
+  set.seed(20260322)
+  dat <- data.frame(x = sort(runif(14)), y = sort(runif(14)))
+
+  bw_short <- np::npcdensbw(
+    y ~ x,
+    data = dat,
+    nomad = TRUE,
+    degree.max = 1L,
+    nmulti = 1L
+  )
+
+  bw_long <- np::npcdensbw(
+    y ~ x,
+    data = dat,
+    regtype = "lp",
+    search.engine = "nomad+powell",
+    degree.select = "coordinate",
+    bernstein.basis = TRUE,
+    degree.min = 0L,
+    degree.max = 1L,
+    degree.verify = FALSE,
+    bwtype = "fixed",
+    nmulti = 1L
+  )
+
+  fit_short <- np::npcdens(
+    y ~ x,
+    data = dat,
+    nomad = TRUE,
+    degree.max = 1L,
+    nmulti = 1L
+  )
+
+  fit_long <- np::npcdens(
+    y ~ x,
+    data = dat,
+    regtype = "lp",
+    search.engine = "nomad+powell",
+    degree.select = "coordinate",
+    bernstein.basis = TRUE,
+    degree.min = 0L,
+    degree.max = 1L,
+    degree.verify = FALSE,
+    bwtype = "fixed",
+    nmulti = 1L
+  )
+
+  expect_lt(max(abs(unname(bw_short$xbw) - unname(bw_long$xbw))), 5e-4)
+  expect_lt(max(abs(unname(bw_short$ybw) - unname(bw_long$ybw))), 5e-4)
+  expect_identical(as.integer(bw_short$degree), as.integer(bw_long$degree))
+  expect_true(is.list(bw_short$nomad.shortcut))
+  expect_equal(fitted(fit_short), fitted(fit_long), tolerance = 5e-4)
+  expect_true(is.list(fit_short$bws$nomad.shortcut))
+})
+
+test_that("npcdist nomad shortcut matches the explicit distribution preset", {
+  skip_if_not_installed("crs")
+
+  old_opts <- options(np.messages = FALSE, np.tree = FALSE)
+  on.exit(options(old_opts), add = TRUE)
+
+  set.seed(20260322)
+  dat <- data.frame(x = sort(runif(14)), y = sort(runif(14)))
+
+  bw_short <- np::npcdistbw(
+    y ~ x,
+    data = dat,
+    nomad = TRUE,
+    degree.max = 1L,
+    nmulti = 1L
+  )
+
+  bw_long <- np::npcdistbw(
+    y ~ x,
+    data = dat,
+    regtype = "lp",
+    search.engine = "nomad+powell",
+    degree.select = "coordinate",
+    bernstein.basis = TRUE,
+    degree.min = 0L,
+    degree.max = 1L,
+    degree.verify = FALSE,
+    bwtype = "fixed",
+    nmulti = 1L
+  )
+
+  fit_short <- np::npcdist(
+    y ~ x,
+    data = dat,
+    nomad = TRUE,
+    degree.max = 1L,
+    nmulti = 1L
+  )
+
+  fit_long <- np::npcdist(
+    y ~ x,
+    data = dat,
+    regtype = "lp",
+    search.engine = "nomad+powell",
+    degree.select = "coordinate",
+    bernstein.basis = TRUE,
+    degree.min = 0L,
+    degree.max = 1L,
+    degree.verify = FALSE,
+    bwtype = "fixed",
+    nmulti = 1L
+  )
+
+  expect_equal(unname(bw_short$xbw), unname(bw_long$xbw), tolerance = 1e-8)
+  expect_equal(unname(bw_short$ybw), unname(bw_long$ybw), tolerance = 1e-8)
+  expect_identical(as.integer(bw_short$degree), as.integer(bw_long$degree))
+  expect_true(is.list(bw_short$nomad.shortcut))
+  expect_equal(fitted(fit_short), fitted(fit_long), tolerance = 1e-8)
+  expect_true(is.list(fit_short$bws$nomad.shortcut))
+})
+
+test_that("npcdens and npcdist nomad shortcuts fail fast on incompatible settings", {
+  x <- data.frame(x = seq(0, 1, length.out = 8))
+  y <- data.frame(y = seq(0, 1, length.out = 8))
+
+  expect_error(
+    np::npcdensbw(xdat = x, ydat = y, nomad = TRUE, regtype = "ll"),
+    "nomad=TRUE requires regtype='lp'"
+  )
+  expect_error(
+    np::npcdensbw(xdat = x, ydat = y, nomad = TRUE, bernstein.basis = FALSE),
+    "requires bernstein.basis=TRUE"
+  )
+  expect_error(
+    np::npcdistbw(xdat = x, ydat = y, nomad = TRUE, bwtype = "adaptive_nn"),
+    "requires bwtype='fixed'"
+  )
+  expect_error(
+    np::npcdistbw(xdat = x, ydat = y, nomad = TRUE, search.engine = "cell"),
+    "requires search.engine='nomad' or 'nomad\\+powell'"
+  )
+})
