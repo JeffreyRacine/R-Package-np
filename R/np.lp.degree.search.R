820e08634ecf51f2d3081ab992b5a267689a2e24
Jeffrey Racine <racinej@mcmaster.ca>
2026-04-01 18:00:52 +0200

    Fix RStudio NOMAD/Powell handoff progress

--- R/np.lp.degree.search.R
+++ R/np.lp.degree.search.R
@@ -1291,6 +1291,20 @@
   state$nomad_best_record <- best_record
   state$start_note_pending <- FALSE
 
+  if (identical(state$renderer, "single_line") &&
+      isTRUE(state$capability$rstudio)) {
+    if (isTRUE(state$rendered)) {
+      con <- .np_progress_single_line_connection()
+      base::cat("\n", file = con, sep = "")
+      flush(con)
+      flush.console()
+    }
+    state$renderer <- "legacy"
+    state$rendered <- FALSE
+    state$last_render_width <- 0L
+    .np_progress_registry$last_single_line_width <- 0L
+  }
+
   line <- .np_progress_format_line(
     state = state,
     done = state$last_done,
