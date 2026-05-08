105e3e2c09ca7e1ab81ab07798dd4a0354c497b2
Jeffrey Racine <racinej@mcmaster.ca>
2026-05-08 09:18:25 -0400

    Modernize plot argument interface

--- tests/testthat/test-plot-warning-interface-contract.R
+++ tests/testthat/test-plot-warning-interface-contract.R
@@ -77,3 +77,62 @@ test_that("common plot option validation reports current public names", {
   expect_false(grepl("plot.errors.type", msg, fixed = TRUE))
   expect_false(grepl("plot.errors.method", msg, fixed = TRUE))
 })
+
+test_that("snake_case plot controls normalize to engine controls", {
+  normalize <- getFromNamespace(".np_plot_normalize_public_dots", "np")
+
+  dots <- normalize(
+    list(
+      output = "both",
+      data_overlay = FALSE,
+      data_rug = TRUE,
+      layout = "current",
+      factor_boxplot = TRUE,
+      boxplot_outliers = FALSE,
+      coef_index = 2L,
+      gradient_order = c(1L, 2L),
+      common_scale = FALSE,
+      proper_method = "isotonic",
+      proper_control = list(mode = "slice"),
+      errors = "asymptotic",
+      render_control = np_render_control(style = "bar", bar = "I", bar_num = 7L)
+    )
+  )
+
+  expect_identical(dots$plot.errors.method, "asymptotic")
+  expect_identical(dots$plot.behavior, "plot-data")
+  expect_false(dots$plot.data.overlay)
+  expect_true(dots$plot.rug)
+  expect_false(dots$plot.par.mfrow)
+  expect_true(dots$plot.bxp)
+  expect_false(dots$plot.bxp.out)
+  expect_identical(dots$coef.index, 2L)
+  expect_identical(dots$gradient.order, c(1L, 2L))
+  expect_false(dots$common.scale)
+  expect_identical(dots$proper.method, "isotonic")
+  expect_identical(dots$proper.control, list(mode = "slice"))
+  expect_identical(dots$plot.errors.style, "bar")
+  expect_identical(dots$plot.errors.bar, "I")
+  expect_identical(dots$plot.errors.bar.num, 7L)
+})
+
+test_that("snake_case plot controls fail cleanly on conflicts", {
+  normalize <- getFromNamespace(".np_plot_normalize_public_dots", "np")
+
+  expect_error(
+    normalize(list(output = "data", plot.behavior = "plot")),
+    "conflicting plot arguments: output and plot.behavior"
+  )
+  expect_error(
+    normalize(list(data_rug = TRUE, plot.rug = FALSE)),
+    "conflicting plot arguments: data_rug and plot.rug"
+  )
+  expect_error(
+    normalize(list(layout = "maybe")),
+    "layout must be one of"
+  )
+  expect_error(
+    np_render_control(bar.num = 3L),
+    "unused argument"
+  )
+})
