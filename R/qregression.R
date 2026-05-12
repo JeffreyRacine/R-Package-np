c4030c9225de9c52a157b07b087c9a4493518c16
Jeffrey Racine <racinej@mcmaster.ca>
2026-05-12 16:57:16 -0400

    Clarify npqreg predict evaluation args

--- R/qregression.R
+++ R/qregression.R
@@ -73,6 +73,8 @@ quantile.qregression <- function(x, ...){ x$quantile }
 predict.qregression <- function(object, se.fit = FALSE, ...) {
   dots <- list(...)
   has.formula.route <- !is.null(object$bws$formula)
+  if (!has.formula.route && !is.null(dots$exdat) && !is.null(dots$newdata))
+    stop("supply only one of 'newdata' or 'exdat' for non-formula qregression prediction")
   if (!has.formula.route && is.null(dots$exdat) && !is.null(dots$newdata)) {
     dots$exdat <- dots$newdata
     dots$newdata <- NULL
