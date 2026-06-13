9ea1f2de2f7721aba1337ddb34f3bf783b93e48f
Jeffrey Racine <racinej@mcmaster.ca>
2026-06-12 21:42:38 -0400

    Clarify optimizer cache summary accounting

--- R/np.plregression.bw.R
+++ R/np.plregression.bw.R
@@ -541,6 +541,7 @@ npplregbw.plbandwidth =
     restart.bandwidth.starts = search_result$restart.bandwidth.starts,
     restart.start.info = search_result$restart.start.info,
     restart.results = search_result$restart.results,
+    nn.cache = search_result$nn.cache,
     trace = search_result$trace
   )
 
@@ -1327,7 +1328,8 @@ npplregbw.default =
           list(
             objective = as.numeric(cell.bws$fval[1L]),
             payload = cell.bws,
-            num.feval = if (!is.null(cell.bws$num.feval)) as.numeric(cell.bws$num.feval[1L]) else NA_real_
+            num.feval = if (!is.null(cell.bws$num.feval)) as.numeric(cell.bws$num.feval[1L]) else NA_real_,
+            nn.cache = cell.bws$nn.cache
           )
         }
 
