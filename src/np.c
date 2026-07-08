318dca62bdab4cb1cbd9d018c3fc86c4814d7b44
Jeffrey Racine <racinej@mcmaster.ca>
2026-07-07 21:15:37 -0400

    Clear regression large-h cache at bw lifecycle

--- src/np.c
+++ src/np.c
@@ -14908,6 +14908,7 @@ static void np_regression_bw_mode(double * runo, double * rord, double * rcon, d
   int scale_cat;
   const char *bw_error_msg = NULL;
 
+  np_cont_largeh_cache_clear_extern();
   num_reg_continuous_extern = myopti[RBW_NCONI];
   num_reg_unordered_extern = myopti[RBW_NUNOI];
   num_reg_ordered_extern = myopti[RBW_NORDI];
@@ -15762,6 +15763,7 @@ cleanup_np_regression_bw_mode:
   }
 
   np_glp_cv_clear_extern();
+  np_cont_largeh_cache_clear_extern();
   np_reg_cv_core_clear_extern();
 
   int_cker_bound_extern = 0;
