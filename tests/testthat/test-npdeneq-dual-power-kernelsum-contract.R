e311c3b320a25c731155d889f676a7c3d0916a01
Jeffrey Racine <racinej@mcmaster.ca>
2026-07-18 11:34:38 -0400

    Test undivided dual-power kernel sums

--- tests/testthat/test-npdeneq-dual-power-kernelsum-contract.R
+++ tests/testthat/test-npdeneq-dual-power-kernelsum-contract.R
@@ -106,6 +106,21 @@ test_that("weighted dual-power sums preserve expanded-sample semantics", {
   expect_identical(weighted$ksum, scalar1$ksum)
   expect_identical(weighted$ksum.power2, scalar2$ksum)
 
+  undivided <- local_eval(dual_sum(
+    bws = bw, txdat = x, leave.one.out = TRUE,
+    bandwidth.divide = FALSE
+  ))
+  undivided1 <- local_eval(npksum(
+    bws = bw, txdat = x, leave.one.out = TRUE,
+    bandwidth.divide = FALSE
+  ))
+  undivided2 <- local_eval(npksum(
+    bws = bw, txdat = x, kernel.pow = 2, leave.one.out = TRUE,
+    bandwidth.divide = FALSE
+  ))
+  expect_identical(undivided$ksum, undivided1$ksum)
+  expect_identical(undivided$ksum.power2, undivided2$ksum)
+
   expanded <- x[rep(seq_len(nrow(x)), counts), , drop = FALSE]
   expanded_sum <- local_eval(dual_sum(
     bws = bw, txdat = expanded, leave.one.out = TRUE,
