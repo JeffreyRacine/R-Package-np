13143915d3d1e5001803ee4c4c53a53dd19b3a25
Jeffrey Racine <racinej@mcmaster.ca>
2026-07-28 23:36:36 -0400

    Reuse adaptive CVLS response convolution tiles

--- src/jksum.c
+++ src/jksum.c
@@ -27527,11 +27527,12 @@ static int np_conditional_density_cvls_lp_adap_block_stream(double *vector_scale
                                                             double *cv){
   const int num_obs = num_obs_train_extern;
   const int block_size = MIN(np_conditional_lp_cvls_block_size(num_obs), MAX(1, num_obs));
-  double **xblock = NULL, **xblock_full = NULL, **yblock = NULL, **yconvblock = NULL;
-  double *quad_cross = NULL;
+  double **loo_work = NULL;
+  double **full_blocks[2] = {NULL, NULL};
+  double **shared_y = NULL;
   NPConditionalXRowCtx xctx = {0};
   NPConditionalYRowCtx yctx = {0}, yconvctx = {0};
-  int i0, j0, ii, jj;
+  int i0, j0, ii, jj, g;
   int status = 1;
 
   if((cv == NULL) || (vector_scale_factor == NULL) || (num_obs <= 0))
@@ -27541,13 +27542,12 @@ static int np_conditional_density_cvls_lp_adap_block_stream(double *vector_scale
   if(int_cyker_bound_extern != 0)
     return 1;
 
-  xblock = alloc_tmatd(num_obs, block_size);
-  xblock_full = alloc_tmatd(num_obs, block_size);
-  yblock = alloc_tmatd(num_obs, block_size);
-  yconvblock = alloc_tmatd(num_obs, block_size);
-  quad_cross = alloc_vecd(block_size*block_size);
-  if((xblock == NULL) || (xblock_full == NULL) || (yblock == NULL) ||
-     (yconvblock == NULL) || (quad_cross == NULL))
+  loo_work = alloc_tmatd(num_obs, block_size);
+  full_blocks[0] = alloc_tmatd(num_obs, block_size);
+  full_blocks[1] = alloc_tmatd(num_obs, block_size);
+  shared_y = alloc_tmatd(num_obs, block_size);
+  if((loo_work == NULL) || (full_blocks[0] == NULL) ||
+     (full_blocks[1] == NULL) || (shared_y == NULL))
     goto cleanup_cvls_lp_adap_block;
 
   if(np_conditional_xrow_ctx_prepare(vector_scale_factor, &xctx) != 0)
@@ -27558,57 +27558,87 @@ static int np_conditional_density_cvls_lp_adap_block_stream(double *vector_scale
     goto cleanup_cvls_lp_adap_block;
 
   *cv = 0.0;
-  for(i0 = 0; i0 < num_obs; i0 += block_size){
-    const int ib = MIN(block_size, num_obs - i0);
-    double lin = 0.0;
-    double quad = 0.0;
+  for(i0 = 0; i0 < num_obs; i0 += 2*block_size){
+    int block_start[2] = {i0, i0 + block_size};
+    int block_rows[2] = {
+      MIN(block_size, num_obs - i0),
+      MIN(block_size, MAX(0, num_obs - i0 - block_size))
+    };
+    double lin[2] = {0.0, 0.0};
+    double quad[2] = {0.0, 0.0};
 
-    for(ii = 0; ii < ib; ii++){
-      const int i = i0 + ii;
-      if(np_lp_engine_extern == NP_LP_ENGINE_GENERAL){
-        if((np_conditional_xrow_full_from_ctx(&xctx,
-                                              i,
-                                              xblock_full[ii]) != 0) ||
-           (np_lp_delete_smoother_row(xblock_full[ii],
-                                      num_obs,
-                                      i,
-                                      xblock[ii]) != 0))
-          goto cleanup_cvls_lp_adap_block;
-      } else {
-        if(np_conditional_xrow_from_ctx(&xctx, i, xblock[ii]) != 0)
-          goto cleanup_cvls_lp_adap_block;
-        if(np_conditional_xrow_full_from_ctx(&xctx, i, xblock_full[ii]) != 0)
+    for(g = 0; g < 2; g++){
+      const int ib = block_rows[g];
+
+      if(ib <= 0)
+        continue;
+      for(ii = 0; ii < ib; ii++){
+        const int i = block_start[g] + ii;
+        if(np_lp_engine_extern == NP_LP_ENGINE_GENERAL){
+          if((np_conditional_xrow_full_from_ctx(&xctx,
+                                                i,
+                                                full_blocks[g][ii]) != 0) ||
+             (np_lp_delete_smoother_row(full_blocks[g][ii],
+                                        num_obs,
+                                        i,
+                                        loo_work[ii]) != 0))
+            goto cleanup_cvls_lp_adap_block;
+        } else {
+          if(np_conditional_xrow_from_ctx(&xctx, i, loo_work[ii]) != 0)
+            goto cleanup_cvls_lp_adap_block;
+          if(np_conditional_xrow_full_from_ctx(&xctx,
+                                               i,
+                                               full_blocks[g][ii]) != 0)
+            goto cleanup_cvls_lp_adap_block;
+        }
+        if(np_conditional_yrow_from_ctx(&yctx, i, shared_y[ii]) != 0)
           goto cleanup_cvls_lp_adap_block;
       }
-      if(np_conditional_yrow_from_ctx(&yctx, i, yblock[ii]) != 0)
-        goto cleanup_cvls_lp_adap_block;
+
+      for(ii = 0; ii < ib; ii++)
+        lin[g] += np_blas_ddot_int(num_obs, loo_work[ii], shared_y[ii]);
     }
 
-    for(ii = 0; ii < ib; ii++)
-      lin += np_blas_ddot_int(num_obs, xblock[ii], yblock[ii]);
-
+    /*
+     * Both LOO linear consumers are complete, so this contiguous slab is
+     * dead and is large enough for the incumbent block_size^2 GEMM output.
+     */
+    double * const quad_cross = loo_work[0];
     for(j0 = 0; j0 < num_obs; j0 += block_size){
       const int jb = MIN(block_size, num_obs - j0);
 
       for(jj = 0; jj < jb; jj++){
         const int j = j0 + jj;
-        if(np_conditional_yrow_from_ctx(&yconvctx, j, yconvblock[jj]) != 0)
+        if(np_conditional_yrow_from_ctx(&yconvctx, j, shared_y[jj]) != 0)
           goto cleanup_cvls_lp_adap_block;
       }
 
-      np_blas_dgemm_tn_int(ib, jb, num_obs, xblock_full[0], yconvblock[0], quad_cross);
-      for(ii = 0; ii < ib; ii++){
-        double * const ai = xblock_full[ii];
-        for(jj = 0; jj < jb; jj++){
-          const double aij = ai[j0 + jj];
-          if(aij == 0.0)
-            continue;
-          quad += aij*quad_cross[ii + jj*ib];
+      for(g = 0; g < 2; g++){
+        const int ib = block_rows[g];
+
+        if(ib <= 0)
+          continue;
+        np_blas_dgemm_tn_int(ib,
+                             jb,
+                             num_obs,
+                             full_blocks[g][0],
+                             shared_y[0],
+                             quad_cross);
+        for(ii = 0; ii < ib; ii++){
+          double * const ai = full_blocks[g][ii];
+          for(jj = 0; jj < jb; jj++){
+            const double aij = ai[j0 + jj];
+            if(aij == 0.0)
+              continue;
+            quad[g] += aij*quad_cross[ii + jj*ib];
+          }
         }
       }
     }
 
-    *cv += quad - 2.0*lin;
+    for(g = 0; g < 2; g++)
+      if(block_rows[g] > 0)
+        *cv += quad[g] - 2.0*lin[g];
   }
 
   *cv /= (double)num_obs;
@@ -27618,11 +27648,10 @@ cleanup_cvls_lp_adap_block:
   np_conditional_xrow_ctx_clear(&xctx);
   np_conditional_yrow_ctx_clear(&yctx);
   np_conditional_yrow_ctx_clear(&yconvctx);
-  if(xblock != NULL) free_tmat(xblock);
-  if(xblock_full != NULL) free_tmat(xblock_full);
-  if(yblock != NULL) free_tmat(yblock);
-  if(yconvblock != NULL) free_tmat(yconvblock);
-  if(quad_cross != NULL) free(quad_cross);
+  if(loo_work != NULL) free_tmat(loo_work);
+  for(g = 0; g < 2; g++)
+    if(full_blocks[g] != NULL) free_tmat(full_blocks[g]);
+  if(shared_y != NULL) free_tmat(shared_y);
   np_glp_cv_clear_extern();
   return status;
 }
