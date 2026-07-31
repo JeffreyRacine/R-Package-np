08de0c10863ca52f298d8a854dc7cdd1e7726913
Jeffrey Racine <racinej@mcmaster.ca>
2026-07-31 09:51:51 -0400

    Move MPI conditional CV oracle into R

--- tests/testthat/test-npcdensbw-cvml-lp-penalty-contract.R
+++ tests/testthat/test-npcdensbw-cvml-lp-penalty-contract.R
@@ -1,63 +1,16 @@
-shadow_conditional_xweights_lp_nprmpi <- function(bw, xdat, ydat, row_index) {
-  make_mat <- function(z) {
-    z <- as.matrix(z)
-    storage.mode(z) <- "double"
-    z
-  }
+independent_lp_delete_one_weights <- function(xdat,
+                                              bandwidth,
+                                              degree,
+                                              row_index) {
+  x <- as.numeric(xdat[[1L]])
+  powers <- 0L:as.integer(degree)
+  basis <- outer(x, powers, `^`)
+  weights <- dnorm((x[[row_index]] - x) / bandwidth)
+  weights[[row_index]] <- 0
 
-  xmat <- npRmpi:::toMatrix(xdat)
-  ymat <- npRmpi:::toMatrix(npRmpi:::toFrame(ydat))
-  ycon <- make_mat(ymat[, bw$iycon, drop = FALSE])
-  yuno <- make_mat(ymat[, bw$iyuno, drop = FALSE])
-  yord <- make_mat(ymat[, bw$iyord, drop = FALSE])
-  xcon <- make_mat(xmat[, bw$ixcon, drop = FALSE])
-  xuno <- make_mat(xmat[, bw$ixuno, drop = FALSE])
-  xord <- make_mat(xmat[, bw$ixord, drop = FALSE])
-
-  bw_vec <- c(
-    bw$xbw[bw$ixcon],
-    bw$ybw[bw$iycon],
-    bw$ybw[bw$iyuno],
-    bw$ybw[bw$iyord],
-    bw$xbw[bw$ixuno],
-    bw$xbw[bw$ixord]
-  )
-
-  .Call(
-    "C_np_shadow_cv_xweights_conditional",
-    yuno,
-    yord,
-    ycon,
-    xuno,
-    xord,
-    xcon,
-    as.double(bw_vec),
-    as.integer(npRmpi:::BW_FIXED),
-    as.integer(switch(
-      bw$cxkertype,
-      gaussian = npRmpi:::CKER_GAUSS + bw$cxkerorder / 2 - 1,
-      epanechnikov = npRmpi:::CKER_EPAN + bw$cxkerorder / 2 - 1,
-      uniform = npRmpi:::CKER_UNI
-    )),
-    as.integer(switch(
-      bw$uxkertype,
-      aitchisonaitken = npRmpi:::UKER_AIT,
-      liracine = npRmpi:::UKER_LR
-    )),
-    as.integer(switch(
-      bw$oxkertype,
-      wangvanryzin = npRmpi:::OKER_WANG,
-      liracine = npRmpi:::OKER_LR,
-      racineliyan = npRmpi:::OKER_RLY
-    )),
-    as.integer(FALSE),
-    as.integer(npRmpi:::REGTYPE_LP),
-    as.integer(bw$degree.engine),
-    as.integer(isTRUE(bw$bernstein.basis.engine)),
-    as.integer(npRmpi:::npLpBasisCode(bw$basis.engine)),
-    as.integer(row_index),
-    PACKAGE = "npRmpi"
-  )$streamed
+  gram <- crossprod(basis, basis * weights)
+  projection <- solve(gram, basis[row_index, ])
+  as.numeric(weights * (basis %*% projection))
 }
 
 bounded_gaussian_kernel_nprmpi <- function(x0, X, h, lower, upper) {
@@ -104,7 +57,12 @@ test_that("npcdensbw cv.ml LP degree-0 bounded objective matches delete-one reco
   )
 
   manual_rows <- vapply(seq_len(n), function(i) {
-    weights <- shadow_conditional_xweights_lp_nprmpi(bw, xdat, ydat, i)
+    weights <- independent_lp_delete_one_weights(
+      xdat,
+      bandwidth = bw$xbw[[1L]],
+      degree = bw$degree.engine[[1L]],
+      row_index = i
+    )
     ky <- bounded_gaussian_kernel_nprmpi(
       ydat[i],
       ydat,
@@ -148,7 +106,12 @@ test_that("npcdensbw cv.ml LP objective uses smooth penalty for negative delete-
   )
 
   manual_rows <- vapply(seq_len(n), function(i) {
-    weights <- shadow_conditional_xweights_lp_nprmpi(bw, xdat, ydat, i)
+    weights <- independent_lp_delete_one_weights(
+      xdat,
+      bandwidth = bw$xbw[[1L]],
+      degree = bw$degree.engine[[1L]],
+      row_index = i
+    )
     ky <- bounded_gaussian_kernel_nprmpi(
       ydat[i],
       ydat,
