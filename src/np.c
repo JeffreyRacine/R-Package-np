3f328544817e33d258275f62925f3c6108998cb7
Jeffrey Racine <racinej@mcmaster.ca>
2026-09-02 12:20:33 -0400

    Recover unconditional density NN searches

--- src/np.c
+++ src/np.c
@@ -13101,6 +13101,9 @@ static void np_density_bw_internal(double * myuno, double * myord, double * myco
   int iMultistart, iMs_counter, iNum_Multistart, iImproved;
   int enforce_fixed_feasibility;
   int have_start_best, have_multistart_best;
+  int nn_retry_attempted = 0;
+  int nn_retry_history_index = 0;
+  int nn_retry_improved = 0;
   int itmax, iter;
   int int_use_starting_values;
   int scale_cat;
@@ -13153,7 +13156,10 @@ static void np_density_bw_internal(double * myuno, double * myord, double * myco
   int_use_starting_values= myopti[BW_USTARTI];
   int_LARGE_SF=myopti[BW_LSFI];
   BANDWIDTH_den_extern=myopti[BW_DENI];
-  enforce_fixed_feasibility = ((BANDWIDTH_den_extern == BW_FIXED) && (!eval_only));
+  enforce_fixed_feasibility =
+    (((BANDWIDTH_den_extern == BW_FIXED) ||
+      (BANDWIDTH_den_extern == BW_GEN_NN) ||
+      (BANDWIDTH_den_extern == BW_ADAP_NN)) && (!eval_only));
   int_RESTART_FROM_MIN = myopti[BW_REMINI];
   int_MINIMIZE_IO = myopti[BW_MINIOI];
 
@@ -13488,6 +13494,7 @@ static void np_density_bw_internal(double * myuno, double * myord, double * myco
     np_copy_scale_factor(vector_scale_factor_startbest, vector_scale_factor, num_var);
   }
 
+density_powell_attempt:
   if(!eval_only){
     powell(0,
            0,
@@ -13579,14 +13586,25 @@ static void np_density_bw_internal(double * myuno, double * myord, double * myco
   *timing = timing_extern;
 
   /* When multistarting save initial minimum of objective function and scale factors */
-  objective_function_values[0]=-fret;
-  objective_function_evals[0]=bwm_eval_count;
-  objective_function_invalid[0]=bwm_invalid_count;
-  bwm_snapshot_fast_counters();
-  fast_eval_total += bwm_fast_eval_count;
-  guarded_eval_total += bwm_guarded_eval_count;
+  if (!nn_retry_attempted) {
+    objective_function_values[0]=-fret;
+    objective_function_evals[0]=bwm_eval_count;
+    objective_function_invalid[0]=bwm_invalid_count;
+    bwm_snapshot_fast_counters();
+    fast_eval_total += bwm_fast_eval_count;
+    guarded_eval_total += bwm_guarded_eval_count;
+  } else {
+    objective_function_values[nn_retry_history_index]=-fret;
+    objective_function_evals[nn_retry_history_index] += bwm_eval_count;
+    objective_function_invalid[nn_retry_history_index] += bwm_invalid_count;
+    bwm_snapshot_fast_counters();
+    fast_eval_total += bwm_fast_eval_count;
+    guarded_eval_total += bwm_guarded_eval_count;
+    iImproved = nn_retry_improved;
+  }
 
-  if((!eval_only) && (iMultistart == IMULTI_TRUE)){
+  if((!eval_only) && (!nn_retry_attempted) &&
+     (iMultistart == IMULTI_TRUE)){
     if (enforce_fixed_feasibility) {
       if (have_start_best) {
         have_multistart_best = 1;
@@ -13809,6 +13827,62 @@ static void np_density_bw_internal(double * myuno, double * myord, double * myco
     }
     final_raw = bwmfunc_raw_current_scale(vector_scale_factor, num_var);
     if (!R_FINITE(final_raw) || final_raw == DBL_MAX) {
+      if ((!nn_retry_attempted) &&
+          (!eval_only) &&
+          (!int_use_starting_values) &&
+          ((BANDWIDTH_den_extern == BW_GEN_NN) ||
+           (BANDWIDTH_den_extern == BW_ADAP_NN)) &&
+          (num_reg_continuous_extern > 0) &&
+          (num_obs_train_extern >= 3)) {
+        double finite_seed = DBL_MAX;
+        int found_finite_seed;
+        const int maximum_k =
+          (BANDWIDTH_den_extern == BW_GEN_NN) ?
+          num_obs_train_extern - 1 : num_obs_train_extern - 2;
+
+        nn_retry_history_index = (iImproved > 0) ? iImproved - 1 : 0;
+        nn_retry_improved = iImproved;
+        bwm_reset_counters();
+        found_finite_seed = np_ordinary_nn_find_finite_raw_seed(
+          vector_scale_factor,
+          num_reg_continuous_extern,
+          num_obs_train_extern,
+          maximum_k,
+          num_var,
+          &finite_seed);
+        if (found_finite_seed &&
+            bwm_penalty_mode == 1 &&
+            (!(bwm_penalty_value > finite_seed))) {
+          found_finite_seed = np_minimized_seed_dominating_penalty(
+            finite_seed, penalty_mult[0], &bwm_penalty_value);
+        }
+        if (found_finite_seed) {
+          initialize_nr_directions(BANDWIDTH_den_extern,
+                                   num_obs_train_extern,
+                                   num_reg_continuous_extern,
+                                   num_reg_unordered_extern,
+                                   num_reg_ordered_extern,
+                                   0,
+                                   0,
+                                   0,
+                                   vsfh,
+                                   num_categories_extern,
+                                   matrix_y,
+                                   0, int_RANDOM_SEED,
+                                   lbc_dir, dfc_dir, c_dir, initc_dir,
+                                   lbd_dir, hbd_dir, d_dir, initd_dir,
+                                   matrix_X_continuous_train_extern,
+                                   matrix_Y_continuous_train_extern);
+          fret_initial = fret_best = fret_start_best = fret = finite_seed;
+          have_start_best = 1;
+          have_multistart_best = 0;
+          np_copy_scale_factor(vector_scale_factor_startbest,
+                               vector_scale_factor,
+                               num_var);
+          nn_retry_attempted = 1;
+          goto density_powell_attempt;
+        }
+      }
       bw_error_msg = "C_np_density_bw: optimizer returned a fixed-bandwidth candidate with invalid raw objective";
       goto cleanup_np_density_bw;
     }
