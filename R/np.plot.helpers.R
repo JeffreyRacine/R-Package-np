bc8c7bcbf5a874b1a3595bc8403ee57e14d38ce5
Jeffrey Racine <racinej@mcmaster.ca>
2026-09-11 12:07:00 -0400

    Limit smooth conditional bootstrap gradient restoration to its target

--- R/np.plot.helpers.R
+++ R/np.plot.helpers.R
@@ -11520,7 +11520,8 @@ compute.default.error.range <- function(center, err) {
       gradient.order = gradient.order,
       lp.first.se.demand = FALSE,
       cat.se.demand = FALSE,
-      se = FALSE
+      se = FALSE,
+      gradient.target = gradient.index
     )
     .np_plot_extract_conditional_gradient(
       fit = fit,
